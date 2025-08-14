1c71b0f7c71232ed0803755d0c6eda4f35289ae1
DESKTOP-FMV3UJA\Admin <ggmukmin@gmail.com>
2025-08-14 13:18:14 +0700

    final bagian admin di folder manajemen_proyek

--- manajemen_proyek/admin/templates/header.php
+++ manajemen_proyek/admin/templates/header.php
@@ -9,15 +9,25 @@ if (!isset($_SESSION['status']) || $_SESSION['status'] != "login") {
 <!doctype html>
 <html lang="id">
 <head>
-  <meta charset="utf-t">
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="../assets/css/pace.min.css" rel="stylesheet" />
+  <script src="../assets/js/pace.min.js"></script>
+
   <link href="../assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
   <link href="../assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
   <link href="../assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+
   <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/css/bootstrap-extended.css" rel="stylesheet">
   <link href="../assets/css/style.css" rel="stylesheet">
   <link href="../assets/css/icons.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+
+  <link href="../assets/css/dark-theme.css" rel="stylesheet" />
+  <link href="../assets/css/semi-dark.css" rel="stylesheet" />
+  <link href="../assets/css/header-colors.css" rel="stylesheet" />
   <title>Manajemen Proyek</title>
 </head>
 <body>
